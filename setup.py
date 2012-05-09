d24978156a012375b1e36367d0c0cad1b240f916
David Winterbottom <david.winterbottom@gmail.com>
2012-05-09 14:17:56 +0100

    Bumped version and updatd docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-oscar-datacash',
-      version='0.2.2',
+      version='0.2.3',
       url='https://github.com/tangentlabs/django-oscar-datacash',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
