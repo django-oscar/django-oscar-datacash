d06f526923a730d20c608c94a815fd604e4e62ee
David Winterbottom <david.winterbottom@gmail.com>
2013-03-20 15:26:44 +0000

    Tidy sandbox view for readability

--- sandbox/apps/checkout/views.py
+++ sandbox/apps/checkout/views.py
@@ -11,23 +11,12 @@ from datacash.facade import Facade
 
 
 class PaymentDetailsView(OscarPaymentDetailsView):
-    
+
     def get_context_data(self, **kwargs):
         # Add bankcard form to the template context
         ctx = super(PaymentDetailsView, self).get_context_data(**kwargs)
         ctx['bankcard_form'] = kwargs.get('bankcard_form', BankcardForm())
         return ctx
-
-    def do_place_order(self, request):
-        bankcard_form = BankcardForm(request.POST)
-        if not bankcard_form.is_valid():
-            messages.error(request, "Invalid submission")
-            return HttpResponseRedirect(reverse('checkout:payment-details'))
-        bankcard = bankcard_form.get_bankcard_obj()
-
-        # Call oscar's submit method, passing through the bankcard object so it gets
-        # passed to the 'handle_payment' method
-        return self.submit(request.basket, payment_kwargs={'bankcard': bankcard})
 
     def post(self, request, *args, **kwargs):
         if request.POST.get('action', '') == 'place_order':
@@ -43,14 +32,25 @@ class PaymentDetailsView(OscarPaymentDetailsView):
         # Render preview page (with bankcard details hidden)
         return self.render_preview(request, bankcard_form=bankcard_form)
 
+    def do_place_order(self, request):
+        bankcard_form = BankcardForm(request.POST)
+        if not bankcard_form.is_valid():
+            messages.error(request, "Invalid submission")
+            return HttpResponseRedirect(reverse('checkout:payment-details'))
+        bankcard = bankcard_form.get_bankcard_obj()
+
+        # Call oscar's submit method, passing through the bankcard object so it
+        # gets passed to the 'handle_payment' method
+        return self.submit(request.basket,
+                           payment_kwargs={'bankcard': bankcard})
+
     def handle_payment(self, order_number, total_incl_tax, **kwargs):
         # Make request to DataCash - if there any problems (eg bankcard
         # not valid / request refused by bank) then an exception would be
         # raised ahd handled)
         facade = Facade()
-        datacash_ref = facade.pre_authorise(order_number,
-                                            total_incl_tax,
-                                            kwargs['bankcard'])
+        datacash_ref = facade.pre_authorise(
+            order_number, total_incl_tax, kwargs['bankcard'])
 
         # Request was successful - record the "payment source".  As this
         # request was a 'pre-auth', we set the 'amount_allocated' - if we had
