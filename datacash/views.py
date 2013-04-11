64075905387b9a7f87fbb81e6fc0d1b88e409102
David Winterbottom <david.winterbottom@gmail.com>
2013-04-11 14:30:32 +0100

    Relocate dashboard templates...
    ...to be consistent with Oscar's naming conventions.

--- datacash/views.py
+++ datacash/views.py
@@ -6,10 +6,10 @@ from datacash.models import OrderTransaction
 class TransactionListView(ListView):
     model = OrderTransaction
     context_object_name = 'transactions'
-    template_name = 'dashboard/datacash/transaction_list.html'
+    template_name = 'datacash/dashboard/transaction_list.html'
 
 
 class TransactionDetailView(DetailView):
     model = OrderTransaction
     context_object_name = 'txn'
-    template_name = 'dashboard/datacash/transaction_detail.html'
+    template_name = 'datacash/dashboard/transaction_detail.html'
